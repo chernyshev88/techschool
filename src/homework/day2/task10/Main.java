8f91c064890bfe7fdf2d847ff32d0d4b9a5dcf0a
alexander.chernyshev <alexander.chernyshev@x5.ru>
2020-09-16 13:21:13 +0300

    Поправил имена переменных. Убрал tmpArray=null;

--- src/homework/day2/task10/Main.java
+++ src/homework/day2/task10/Main.java
@@ -2,24 +2,23 @@ package homework.day2.task10;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ar = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
+        int[] inArray = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
         System.out.print(" input array:");
-        for (int x : ar) {
+        for (int x : inArray) {
             System.out.print(x + " ");
         }
-        int[] temp_ar = new int[ar.length];
-        int tmp_count = 0;
-        for (int i = 0; i < ar.length; i++) {
-            if (ar[i] != 0) {
-                temp_ar[tmp_count] = ar[i];
-                tmp_count++;
+        int[] tmpArray = new int[inArray.length];
+        int tmpCount = 0;
+        for (int i = 0; i < inArray.length; i++) {
+            if (inArray[i] != 0) {
+                tmpArray[tmpCount] = inArray[i];
+                tmpCount++;
             }
         }
-        ar = temp_ar;
-        temp_ar = null;
+        inArray = tmpArray;
         System.out.println();
         System.out.print("output array:");
-        for (int x : ar) {
+        for (int x : inArray) {
             System.out.print(x + " ");
         }
     }
